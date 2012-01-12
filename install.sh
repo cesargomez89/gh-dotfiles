4bd4d4404518672e6daa2b782d46a75562341b6f
Antonio Chavez <cavjzz@gmail.com>
2012-01-12 10:43:54 -0600

    Added color config

--- install.sh
+++ install.sh
@@ -1,2 +1,23 @@
 echo  source ~/.gh-dotfiles/profile >> ~/.profile
 echo  source ~/.gh-dotfiles/profile >> ~/.bashrc
+
+echo [color] >> ~/.gitconfig
+echo   branch = auto >> ~/.gitconfig
+echo   diff = auto >> ~/.gitconfig
+echo   status = auto >> ~/.gitconfig
+  
+echo [color "branch"] >> ~/.gitconfig
+echo   current = white >> ~/.gitconfig
+echo   local = yellow >> ~/.gitconfig
+echo   remote = green >> ~/.gitconfig
+
+echo [color "diff"] >> ~/.gitconfig
+echo   meta = yellow bold >> ~/.gitconfig
+echo   frag = magenta bold >> ~/.gitconfig
+echo   old = red bold >> ~/.gitconfig
+echo   new = green bold >> ~/.gitconfig
+
+echo [color "status"] >> ~/.gitconfig
+echo   added = yellow >> ~/.gitconfig
+echo   changed = green >> ~/.gitconfig
+echo   untracked = red bold >> ~/.gitconfig
